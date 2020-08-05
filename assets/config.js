dd63d21b98b6061f78a17299a0965471923b8ea4
XiuyuLiu <46135164+XiuyuLiu@users.noreply.github.com>
2020-08-04 21:47:29 -0500

    Add files via upload

---
+++ assets/config.js
@@ -0,0 +1,40 @@
+// cpanel - site_templates/vcard_talent/assets/config.js.tt Copyright(c) 2016 cPanel, Inc.
+//                                                          All rights Reserved.
+// copyright@cpanel.net                                        http://cpanel.net
+// This code is subject to the cPanel license. Unauthorized copying is prohibited
+
+window.cpanel = {
+    data: {
+        phone: "2173050302",
+        fax: "",
+        email: "xiuyul2@illinois.edu",
+        address: "",
+        social: [
+            
+            
+            
+            
+            
+            
+        ]
+    },
+    style: {
+        primary: "",
+        accent: ""
+    },
+    slides: [
+        {
+            prefix: "My name is",
+            title: "Xiuyu Liu",
+            subtitle: "",
+            type: 'vcard',
+            backgroundImage: "",
+            backgroundColor: "",
+            color: "",
+            buttonText: "",
+            buttonLink: "",
+            biography: "",
+            portraitImage: ""
+        }
+    ]
+};
